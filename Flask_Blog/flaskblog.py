5de47861053e25f598c7295db9284dbe73ff954a
JieEnT <jieen1000@gmail.com>
2022-06-27 09:52:39 +0800

    User form and input

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+#Secret key protect against forgery, xss attacks
+#Secret key helps to encrypt cookies and send them to the browser
+app.config['SECRET_KEY'] = '8c3fb25a345726fc6442e19a29967a71'
 
 #A list of all the possible posts
 posts = [
@@ -26,6 +31,23 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        #f string is a literal string
+        flash(f'Account created for {form.username.data}!', 'success')
+        #We will redirct to the home page after submission of form
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 # If we run the script directly from python then the name of the module is equivalent to __main__
